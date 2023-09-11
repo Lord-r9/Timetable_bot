0cf4e5a53bb564a695d00a4e00078a32e33d62f5
Eisim <koctik.w@mail.ru>
2023-09-11 21:40:11 +0300

    Append lessons time.Now can delete compilted days

--- TrelloConnector.py
+++ TrelloConnector.py
@@ -1,6 +1,20 @@
 import requests as rq
 import config
+import datetime
 
+def day_in_list(day,lists,day_format="%Y.%m.%d",list_format="%Y.%m.%d"):
+    day=str_to_date(day,day_format)
+    for list in lists:
+        if str_to_date(list['name'][3:],format=list_format) == day:
+            return True
+    return False
+def action_is_complited(end, format="%Y-%m-%dT%H:%M:%S.%fZ"):
+    end = datetime.datetime.strptime(end,format)
+    now = datetime.datetime.now()
+    res = end - now < datetime.timedelta(0)
+    return res
+def str_to_date(date,format="%Y-%m-%dT%H:%M:%S.%fZ"):
+    return datetime.datetime.strptime(date,format)
 
 class TrelloConnector:
     def __init__(self, board_id):
@@ -27,14 +41,15 @@ class TrelloConnector:
         self.response = self.session.get(url, headers=self.headers, params=self.params)
         return self.response.json()
 
-    def create_list(self, name):
+    def list_name_generator(self, description):
+        return str(description["dayOfWeek"] + " " + description["date"])
+
+    def create_list(self, description,pos='bottom'):
         url = f"https://api.trello.com/1/lists"
-        params = {
-            'name': f'{name}',
-            'idBoard': self._board_id,
-            'key': self.params['key'],
-            'token': self.params['token']
-        }
+        params = self.params
+        params['idBoard']=self._board_id
+        params['name']=self.list_name_generator(description)
+        params['pos']=pos
         self.response = self.session.post(url, params=params)
         return self.response.json()
 
@@ -43,13 +58,14 @@ class TrelloConnector:
         params = self.params
         params['value'] = "true"
         self.response = self.session.put(url, params=params)
+
     def archive_all_lists(self):
         [self.archive_list(list["id"]) for list in self.get_lists_on_a_board()]
 
     def get_color_id(self, lesson):
-        colors=config.COLORS
+        colors = config.COLORS
         for label in self.get_labels():
-            if label['color'] == colors[lesson] and label['name']==str(lesson):
+            if label['color'] == colors[lesson] and label['name'] == str(lesson):
                 return label['id']
         raise Exception(f"Color not found(kind:{lesson})")
 
@@ -64,23 +80,76 @@ class TrelloConnector:
         params['name'] = name
         params['color'] = color
         self.response = self.session.put(url, params=params)
-    def create_label(self,board_id,name,color):
-        url=f"https://api.trello.com/1/labels"
-        params=self.params
-        params['name']=name
-        params['color']=color
-        params['idBoard']=board_id
-        self.response=self.session.post(url,params=params)
+
+    def create_label(self, name, color):
+        url = f"https://api.trello.com/1/labels"
+        params = self.params
+        params['name'] = name
+        params['color'] = color
+        params['idBoard'] = self._board_id
+        self.response = self.session.post(url, params=params)
+
+    def date_to_trello_format(self, date):
+        return date.replace('.', '-')
+
+    def card_name_generator(self, description):
+        return f"{description['beginLesson']}-{description['endLesson']}: " \
+               f"{description['discipline']} " \
+               f"{description['auditorium']}({description['building']})"
+
     def create_card(self, idList, description):
         url = "https://api.trello.com/1/cards"
         params = self.params
         params["idList"] = idList
-        params["name"] = f"{description['beginLesson']}-{description['endLesson']}: " \
-                         f"{description['discipline']}"
-        params["desc"] = f"{description['beginLesson']}-{description['endLesson']}: " \
-                         f"{description['discipline']}" \
-                         f"({description['kindOfWork']}) " \
-                         f"аудитория{description['auditorium']}({description['building']})"
+        params["name"] = self.card_name_generator(description)
+        params["desc"] = f"Преподаватель: {description['lecturer']}"
+
+        date = self.date_to_trello_format(description['date'])
+        params["start"] = date + "T" + description["beginLesson"] + "Z"
+        params["due"] = date + "T" + description["endLesson"] + "Z"
+
         params["idLabels"] = self.get_color_id(description['kindOfWork'])
-        params["pos"]="top"
+        params["pos"] = "bottom"
         self.response = self.session.post(url, headers=self.headers, params=params)
+
+    def get_card(self, card_id):
+        url = f"https://api.trello.com/1/cards/{card_id}"
+        self.response = self.session.get(url, headers=self.headers, params=self.params)
+        return self.response.json()
+
+    def get_cards_on_list(self, list_id):
+        url = f"https://api.trello.com/1/lists/{list_id}/cards"
+        self.response = self.session.get(url=url, headers=self.headers, params=self.params)
+        return self.response.json()
+
+    def update_card_status(self, card_id):
+        url = f"https://api.trello.com/1/cards/{card_id}"
+        card_is_closed=action_is_complited(self.get_card(card_id)['due'])
+        params = self.params
+        params['dueComplete'] = str(card_is_closed)
+        self.response = self.session.put(url=url, headers=self.headers, params=params)
+        return card_is_closed
+    def archive_complited_list(self, list_id):
+        url=f"https://api.trello.com/1/lists/{list_id}"
+        cards = self.get_cards_on_list(list_id)
+        for card in cards:
+            if not self.update_card_status(card['id']):
+                return False
+        params=self.params
+        params['closed']='true'
+        self.response=self.session.put(url=url,params=params)
+
+    def archive_complited_lists(self):
+        list_id = [list["id"] for list in self.get_lists_on_a_board()]
+        for list in list_id:
+            self.archive_complited_list(list)
+    def append_missing_days(self,table):
+
+       ###TODO
+       ###TODO
+       ###TODO
+
+       pass
+    def update_table(self,table):
+        self.archive_complited_lists()
+        self.append_missing_days(table)
