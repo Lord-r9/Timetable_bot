21e3efd06974b8974233533253d74e4e86ad58a3
Eisim <koctik.w@mail.ru>
2023-09-12 00:05:43 +0300

    .

--- config.py
+++ config.py
@@ -16,4 +16,4 @@ UNN_PASSWORD = os.getenv("UNN_PASSWORD")
 
 API_KEY_TRELLO = os.getenv("API_KEY_TRELLO")
 API_TOKEN_TRELLO = os.getenv("API_TOKEN_TRELLO")
-BOARD_ID = os.getenv("BOARD_ID_TYDS")
+BOARD_ID = os.getenv("BOARD_ID_PM")
