fa5ecbb678cd22c25368d1d0a9f4d54471c8b267
Eisim <koctik.w@mail.ru>
2023-09-11 21:40:52 +0300

    minor fix

--- config.py
+++ config.py
@@ -16,5 +16,4 @@ UNN_PASSWORD = os.getenv("UNN_PASSWORD")
 
 API_KEY_TRELLO = os.getenv("API_KEY_TRELLO")
 API_TOKEN_TRELLO = os.getenv("API_TOKEN_TRELLO")
-BOARD_ID = os.getenv("BOARD_ID")
-ACTION_ID = ""
+BOARD_ID = os.getenv("BOARD_ID_TYDS")
